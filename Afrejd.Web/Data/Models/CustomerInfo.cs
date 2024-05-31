d364342afab1165f68347764a987010be41a33eb
QeepyWebDev <145613539+QeepyWebDev@users.noreply.github.com>
2024-05-31 16:36:09 +0200

    Fixed customerInfo null error.

--- Afrejd.Web/Data/Models/CustomerInfo.cs
+++ Afrejd.Web/Data/Models/CustomerInfo.cs
@@ -2,17 +2,17 @@
 {
     public class CustomerInfo
     {
-        public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Address { get; set; }
-        public string City { get; set; }
-        public string Region { get; set; }
-        public int PostalCode { get; set; }
-        public string Country { get; set; }
-        public int Phone { get; set; }
-        public string Company { get; set; }
-        public string OrganizationNumber { get; set; }
+        public int Id { get; set; } 
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string Region { get; set; } = string.Empty;
+        public int ? PostalCode { get; set; }
+        public string Country { get; set; } = string.Empty;
+        public int ? Phone { get; set; }
+        public string Company { get; set; } = string.Empty;
+        public string OrganizationNumber { get; set; } = string.Empty;
         public string UserId { get; set; }
         public ApplicationUser User { get; set; }
         public ICollection<Order> Orders { get; set; }
